a631bc2f904134483dcd5038f760974d5e0360fb
unknown <Niromedium@gmail.com>
2022-08-27 16:57:36 +0500

    add Задача18

---
+++ Case18/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите цифру от 1 до 7, обозначающую день недели, чтобы узнать, является ли этот день выходным:");
+int a = int.Parse(Console.ReadLine());
+
+while (a < 1 || a > 7)
+{
+    Console.WriteLine("Введите цифру от 1 до 7:");
+    a = int.Parse(Console.ReadLine());
+}
+
+if (a > 0 && a < 6)
+{
+    Console.WriteLine(a + "-> рабочий");
+}
+else
+{
+    Console.WriteLine(a + "-> выходной");
+}
