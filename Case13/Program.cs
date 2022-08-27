5887b86f22f29cbabd1e33c4024f12f85729454a
unknown <Niromedium@gmail.com>
2022-08-27 16:47:07 +0500

    add Задача13

---
+++ Case13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int i = 1000;
+int n = 1;
+
+if (a < 100)
+{
+    Console.WriteLine(a + "-> нет третьей цифры");
+}
+else
+{
+    while (a > i)
+    {
+        i = i*10;
+        n = n*10;
+
+    }
+    Console.WriteLine((a/n)%10 + "-> третья цифра числа");
+}
